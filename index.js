06fe02c5cf9cfcaf0c8fa262286eceda92770cd6
Micael Franco <micael.franco@aventus.io>
2023-03-16 16:08:59 +0000

    remove comment line

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ AvnApi.prototype.init = async function () {
   this.proxy = Proxy;
   this.utils = Utils;
 
-  // TODO: do we want to allow changing SURI on the fly?
   const getSuri = () => {
     this.options.suri = this.options.suri ?? process.env.AVN_SURI;
     return this.options.suri;
