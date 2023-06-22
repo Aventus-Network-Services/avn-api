0d44711b88bf790f8b14ac6389c37aea751eaf64
nahuseyoum <nahu.seyoum@aventus.io>
2023-06-22 19:06:01 +0100

    update public key to always be formatted as hex

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ AvnApi.prototype.init = async function () {
 
   this.signer = () => (apiHasRemoteSigner(this.options) ? this.options.signer : Utils.getSigner(this.options.suri));
   this.myAddress = () => this.signer().address;
-  this.myPublicKey = () => this.signer().publicKey;
+  this.myPublicKey = () => Utils.convertToHexIfNeeded(this.signer().publicKey);
 
   if (this.gateway) {
     this.awtToken = await Awt.generateAwtToken(this.options, this.signer());
