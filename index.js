944fb814ca6e6c72cb7d6b2d430790351ef5c6c6
nahuseyoum <nahu.seyoum@aventus.io>
2023-07-06 01:40:54 +0100

    remove debug logs

--- index.js
+++ index.js
@@ -58,12 +58,9 @@ AvnApi.prototype.init = async function () {
       throw new Error('Signer must be an object with a sign function and an address function');
     }
 
-    console.info('\t - Setting signer: ', this.options.signer.address);
     this.options.suri = undefined;
 
     updateRemoteSignFunction(signer);
-
-    console.info('\t - New signer: ', this.options.signer.sign.toString());
 
     this.awtToken = this.gateway ? await Awt.generateAwtToken(this.options, this.options.signer) : undefined;
     console.info('\t - Signer updated');
