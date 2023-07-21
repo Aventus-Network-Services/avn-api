b47c17bd022817f0f990ec9cba6d0581ff161fd6
nahuseyoum <nahu.seyoum@aventus.io>
2023-07-21 16:00:05 +0100

    wip

--- lib/caching/inMemoryNonceCacheProvider.ts
+++ lib/caching/inMemoryNonceCacheProvider.ts
@@ -1,5 +1,6 @@
 import { CachedNonceInfo, INonceCacheProvider, NonceData } from './index';
 import { NonceType } from '../interfaces';
+import { AccountUtils } from '../utils';
 
 export class InMemoryNonceCacheProvider implements INonceCacheProvider {
   private nonceMap: { [x: string]: { [x: string]: NonceData } };
@@ -12,16 +13,19 @@ export class InMemoryNonceCacheProvider implements INonceCacheProvider {
   }
 
   async initUserNonceCache(signerAddress: string): Promise<void> {
+    signerAddress = AccountUtils.addressToPublicKey(signerAddress);
     if (this.nonceMap[signerAddress] === undefined) {
         this.nonceMap[signerAddress] = { nonceType: undefined };
     }
   }
 
   async isNonceLocked(signerAddress: string, nonceType: NonceType): Promise<boolean> {
+    signerAddress = AccountUtils.addressToPublicKey(signerAddress);
     return this.nonceMap[signerAddress][nonceType].locked;
   }
 
   async getNonceAndLock(signerAddress: string, nonceType: NonceType): Promise<CachedNonceInfo> {
+    signerAddress = AccountUtils.addressToPublicKey(signerAddress);
     const nonceData = this.nonceMap[signerAddress][nonceType];
     if (nonceData.locked === false) {
       const lockId = this.getLockId(signerAddress, nonceType, nonceData.nonce);
@@ -34,6 +38,7 @@ export class InMemoryNonceCacheProvider implements INonceCacheProvider {
   }
 
   async incrementNonce(lockId: string, signerAddress: string, nonceType: NonceType, updateLastUpdate: boolean): Promise<NonceData> {
+    signerAddress = AccountUtils.addressToPublicKey(signerAddress);
     if (this.nonceMap[signerAddress][nonceType].locked !== true || this.nonceMap[signerAddress][nonceType].lockId != lockId) {
         throw new Error(`Invalid attempt to increment lock. LockId: ${lockId}, signerAddress: ${signerAddress}, nonceType: ${nonceType}`)
     }
@@ -47,11 +52,13 @@ export class InMemoryNonceCacheProvider implements INonceCacheProvider {
   }
 
   async unlockNonce(signerAddress: string, nonceType: NonceType): Promise<void> {
+    signerAddress = AccountUtils.addressToPublicKey(signerAddress);
     this.nonceMap[signerAddress][nonceType].locked = false;
     this.nonceMap[signerAddress][nonceType].lockId = undefined;
   }
 
   async setNonce(signerAddress: string, nonceType: NonceType, nonce: number): Promise<void> {
+    signerAddress = AccountUtils.addressToPublicKey(signerAddress);
     if (this.nonceMap[signerAddress][nonceType].locked === true) {
         throw new Error(`Nonce is locked`);
     }
