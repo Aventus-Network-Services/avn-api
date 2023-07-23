d3aa4dda512318ba1c12fc37c1e48a7f44fe6f0d
nahuseyoum <nahu.seyoum@aventus.io>
2023-07-23 20:04:10 +0100

    wip

--- lib/caching/inMemoryLock.ts
+++ lib/caching/inMemoryLock.ts
@@ -8,13 +8,16 @@ export default class InMemoryLock {
   }
 
   lock(key: string): Promise<void> {
+    console.log(` - L -Locking ${key}`)
     return new Promise(resolve => {
       const request = () => {
+        console.log(` - L -[UnLocking ${key}]`)
         this.locks[key] = true;
         resolve();
       };
 
       if (this.locks[key] === true) {
+        console.log(` - L -${key} added to lock Q`)
         this.requestQueue.push(request);
       } else {
         // Wait for 1 sec to give the transaction time to be sent to the chain
