45d8ddf2bfd5a7c4b24cf42099e19b9c6559f5bf
nahuseyoum <nahu.seyoum@aventus.io>
2023-07-23 18:09:53 +0100

    wip

--- lib/caching/inMemoryLock.ts
+++ lib/caching/inMemoryLock.ts
@@ -1,5 +1,6 @@
 export default class InMemoryLock {
-  private locks: { [key: string]: { isLocked: boolean; requestQueue: (() => void)[] } };
+  private locks: { [key: string]: boolean };
+  private requestQueue: (() => void)[];
 
   constructor() {
     this.locks = {};
@@ -7,31 +8,25 @@ export default class InMemoryLock {
 
   lock(key: string): Promise<void> {
     return new Promise(resolve => {
-      if (!this.locks[key]) {
-        this.locks[key] = { isLocked: false, requestQueue: [] };
-      }
-
       const request = () => {
-        this.locks[key].isLocked = true;
+        this.locks[key] = true;
         resolve();
       };
 
-      if (this.locks[key].isLocked) {
-        this.locks[key].requestQueue.push(request);
+      if (this.locks[key] === true) {
+        this.requestQueue.push(request);
       } else {
-        request();
+        // Wait for 1 sec to give the transaction time to be sent to the chain
+        setTimeout(() => {
+            request();
+        }, 1000);
       }
     });
   }
 
   unlock(key: string) {
-    const lock = this.locks[key];
-    if (!lock) {
-      throw new Error(`No lock found for key: ${key}`);
-    }
-
-    lock.isLocked = false;
-    const nextRequest = lock.requestQueue.shift();
+    this.locks[key] = false;
+    const nextRequest = this.requestQueue.shift();
     if (nextRequest) {
       nextRequest();
     }
