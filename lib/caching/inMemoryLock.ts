eaed335686729be98e35962883e6f779c37603d3
nahuseyoum <nahu.seyoum@aventus.io>
2023-07-24 03:12:07 +0100

    wip

--- lib/caching/inMemoryLock.ts
+++ lib/caching/inMemoryLock.ts
@@ -1,25 +1,27 @@
 export default class InMemoryLock {
   private sameUserNonceDelayMs: number;
-  private locks: { [key: string]: boolean };
-  private requestQueue: (() => void)[];
+  private locks: { [key: string]: { isLocked: boolean; requestQueue: (() => void)[] } };
 
   constructor(sameUserNonceDelayMs: number) {
     this.locks = {};
-    this.requestQueue = [];
     this.sameUserNonceDelayMs = sameUserNonceDelayMs;
   }
 
   lock(key: string): Promise<void> {
     return new Promise(resolve => {
+      if (!this.locks[key]) {
+        this.locks[key] = { isLocked: false, requestQueue: [] };
+      }
+
       const request = () => {
         console.log(` - L -Locking ${key}`);
-        this.locks[key] = true;
+        this.locks[key].isLocked = true;
         resolve();
       };
 
-      if (this.locks[key] === true) {
+      if (this.locks[key].isLocked) {
         console.log(` - L -${key} added to lock Q`);
-        this.requestQueue.push(request);
+        this.locks[key].requestQueue.push(request);
       } else {
         request();
       }
@@ -28,10 +30,16 @@ export default class InMemoryLock {
 
   async unlock(key: string) {
     console.log(` - L - Calling Unlock for: ${key}`);
-    const nextRequest = this.requestQueue.shift();
 
+    const lock = this.locks[key];
+
+    if (!lock) {
+      throw new Error(`No lock found for key: ${key}`);
+    }
+
+    const nextRequest = lock.requestQueue.shift();
     if (!nextRequest) {
-        this.locks[key] = false;
+        lock.isLocked = false;
         return;
     }
 
@@ -39,7 +47,7 @@ export default class InMemoryLock {
     await new Promise(resolve => setTimeout(resolve, this.sameUserNonceDelayMs));
     console.log("....unlock done: ", new Date())
 
-    this.locks[key] = false;
+    lock.isLocked = false;
     // We are here because multiple requests for the same user and nonce were attempted so create a delay
     // to prevent the nonce going to the gateway out of order
     nextRequest();
