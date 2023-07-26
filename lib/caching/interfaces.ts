5f28668163ab0a3840d000c6366497bb2b167804
nahuseyoum <nahu.seyoum@aventus.io>
2023-07-26 19:15:53 +0100

    Introduce types

---
+++ lib/caching/interfaces.ts
@@ -0,0 +1,26 @@
+export enum NonceCacheType {
+  Local,
+  Remote
+}
+
+export interface CachedNonceInfo {
+  lockAquired: boolean;
+  data: NonceData;
+}
+
+export interface NonceData {
+  nonce: number;
+  lastUpdated: number;
+  locked: boolean;
+  lockId?: string;
+}
+
+export interface INonceCacheProvider {
+  connect(): Promise<INonceCacheProvider>;
+  initUserNonceCache(signerAddress: string): Promise<void>;
+  getNonceAndLock(signerAddress: string, nonceType: string): Promise<CachedNonceInfo>;
+  getNonceData(signerAddress: string, nonceType: string): Promise<NonceData>;
+  incrementNonce(lockId: string, signerAddress: string, nonceType: string, updateLastUpdate: boolean): Promise<NonceData>;
+  unlockNonce(signerAddress: string, nonceType: string): Promise<void>;
+  setNonce(lockId: string, signerAddress: string, nonceType: string, nonce: number): Promise<void>;
+}
---
+++ lib/utils/index.ts
@@ -0,0 +1,48 @@
+import { TypeRegistry } from '@polkadot/types';
+import { Keyring } from '@polkadot/keyring';
+
+export * from './accountUtils';
+export * from './utils';
+
+export const registry = new TypeRegistry();
+export const keyring = new Keyring({ type: 'sr25519', ss58Format: 42 });
+
+export enum TxType {
+  ProxyAvtTransfer = 'proxyAvtTransfer',
+  ProxyTokenTransfer = 'proxyTokenTransfer',
+  ProxyConfirmTokenLift = 'proxyConfirmTokenLift',
+  ProxyTokenLower = 'proxyTokenLower',
+  ProxyCreateNftBatch = 'proxyCreateNftBatch',
+  ProxyMintSingleNft = 'proxyMintSingleNft',
+  ProxyMintBatchNft = 'proxyMintBatchNft',
+  ProxyListNftOpenForSale = 'proxyListNftOpenForSale',
+  ProxyListNftBatchForSale = 'proxyListNftBatchForSale',
+  ProxyTransferFiatNft = 'proxyTransferFiatNft',
+  ProxyCancelListFiatNft = 'proxyCancelListFiatNft',
+  ProxyEndNftBatchSale = 'proxyEndNftBatchSale',
+  proxyStakeAvt = 'proxyStakeAvt',
+  ProxyIncreaseStake = 'proxyIncreaseStake',
+  ProxyUnstake = 'proxyUnstake',
+  ProxyWithdrawUnlocked = 'proxyWithdrawUnlocked',
+  ProxyScheduleLeaveNominators = 'proxyScheduleLeaveNominators',
+  ProxyExecuteLeaveNominators = 'proxyExecuteLeaveNominators'
+}
+
+export enum EthereumLogEventType {
+  AddedValidator = 0,
+  Lifted = 1,
+  NftMint = 2,
+  NftTransferTo = 3,
+  NftCancelListing = 4,
+  NftCancelBatchListing = 5
+}
+
+export enum Market {
+  Ethereum = 1,
+  Fiat = 2
+}
+
+export enum StakingStatus {
+  isStaking = 'isStaking',
+  isNotStaking = 'isNotStaking'
+}
