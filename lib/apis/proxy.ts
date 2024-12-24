855a71a398d26748bafbb2481a0a706cad1eab21
nahuseyoum <nahu.seyoum@aventus.io>
2024-12-24 15:39:23 +0000

    update

--- lib/apis/proxy.ts
+++ lib/apis/proxy.ts
@@ -468,7 +468,7 @@ async function signProxyCreateMarketAndDeployPool({
       { AccountId: dataRelayer },
       { u64: nonce },
       { AssetOf: baseAsset },
-      { BalanceOf: creatorFee },
+      { Perbill: creatorFee },
       { AccountId: validatedOracle },
       { MarketPeriodOf: period },
       { DeadlinePeriodOf: deadlines },
