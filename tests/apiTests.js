9c79053f930601eefcfb5c9f5ee461ba50c3b45b
Rashmi <rashmi.sheshadri@aventus.io>
2025-11-10 10:37:01 +0000

    Basic test structure

---
+++ tests/apiTests.js
@@ -0,0 +1,20 @@
+
+//  require('./configurationTests.js');
+// require('./accountFunding.js');
+// require('./apiOffline.js');
+//require('./accessTests.js');
+//  require('./awtTest.js');
+//  require('./utilsTest.js');
+// require('./pollApiTest.js');
+//require('./proxyApiTest.js');
+//require('./queryApiTest.js');
+// require('./queryApiTest/contractAddress.js')
+require('./queryApiTest/totals.js')
+//  require('./sendApiTest.js');
+// require('./relayerFees.js');
+// require('./splitFees.js');
+// require('./stakingTests.js');
+// require('./remoteSignerTests.js');
+// require('./failPollApiTest.js');
+// require('./failQueryApiTests.js');
+// require('./failSendApiTest.js');
